64591f98ca220d573649f9c29dbcb193e76a597d
timejp <tokijh@naver.com>
2017-03-26 21:43:20 +0900

    Adapter for showing Fragment in viewPager (See #4)

--- app/src/main/java/com/team3/fastcampus/record/Diary/DiaryViewFragment.java
+++ app/src/main/java/com/team3/fastcampus/record/Diary/DiaryViewFragment.java
@@ -7,6 +7,8 @@ package com.team3.fastcampus.record.Diary;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +25,9 @@ public class DiaryViewFragment extends Fragment {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
+    private DiaryListViewFragment diaryListViewFragment;
+    private DiaryMapViewFragment diaryMapViewFragment;
 
 
     public DiaryViewFragment() {
@@ -45,4 +50,36 @@ public class DiaryViewFragment extends Fragment {
         viewPager = (ViewPager) view.findViewById(R.id.viewPager);
     }
 
+    private void initFragment() {
+        diaryListViewFragment = new DiaryListViewFragment();
+        diaryMapViewFragment = new DiaryMapViewFragment();
+    }
+
+    class DiaryViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public static final int TAB_COUNT = 2;
+
+        public DiaryViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0: // Show List
+                    fragment = diaryListViewFragment;
+                    break;
+                case 1: // Show Map
+                    fragment = diaryMapViewFragment;
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
+    }
 }
