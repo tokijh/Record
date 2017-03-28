6dfbaa88949290e2e433ef44da8d009e871205bf
kyuwankim <lpnids@gmail.com>
2017-03-28 17:19:53 +0900

    Add FacebookLogin

--- app/src/main/java/com/team3/fastcampus/record/Account/SigninActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/Account/SigninActivity.java
@@ -7,11 +7,20 @@ package com.team3.fastcampus.record.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.team3.fastcampus.record.R;
+
+import org.json.JSONObject;
 
 /**
  * 회원 로그인 Activity
@@ -25,16 +34,56 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-
-        loginButton = (LoginButton) findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
 
-    }
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile", "email");
+        Log.d("TAG","이바인딩영역=============");
 
+        // kyuwan :  내가보기엔 이부분이 아예 먹히질 않는거같어
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("토큰", loginResult.getAccessToken().getToken());
+                Log.d("유저아이디", loginResult.getAccessToken().getUserId());
+                Log.d("퍼미션 리스트", loginResult.getAccessToken().getPermissions() + "");
+                Log.d("TAG","이거 되고있는거 맞음?");
+                Toast.makeText(SigninActivity.this, "id : " +loginResult.getAccessToken().getUserId(), Toast.LENGTH_SHORT).show();
+
+                //loginResult.getAccessToken() 정보를 가지고 유저 정보를 가져올수 있습니다.
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    Log.e("user profile", object.toString());
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
 
     public void btnSignup(View view) {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
-
 }
+
+
+
