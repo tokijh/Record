174a07d24273feefa8b7c80d6d6c88ce5e7fe96d
Yoon Joonghyun <tokijh@naver.com>
2017-03-29 00:53:46 +0900

    Log 동작 변경
    Logger의 동작을 변경 시킨다.

--- app/src/main/java/com/team3/fastcampus/record/Account/SigninActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/Account/SigninActivity.java
@@ -7,9 +7,7 @@ package com.team3.fastcampus.record.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -19,6 +17,7 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.team3.fastcampus.record.R;
+import com.team3.fastcampus.record.Util.Logger;
 
 import org.json.JSONObject;
 
@@ -38,17 +37,15 @@ public class SigninActivity extends AppCompatActivity {
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("public_profile", "email");
-        Log.d("TAG","이바인딩영역=============");
 
         // kyuwan :  내가보기엔 이부분이 아예 먹히질 않는거같어
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.d("토큰", loginResult.getAccessToken().getToken());
-                Log.d("유저아이디", loginResult.getAccessToken().getUserId());
-                Log.d("퍼미션 리스트", loginResult.getAccessToken().getPermissions() + "");
-                Log.d("TAG","이거 되고있는거 맞음?");
-                Toast.makeText(SigninActivity.this, "id : " +loginResult.getAccessToken().getUserId(), Toast.LENGTH_SHORT).show();
+                Logger.d("SigninActivity", "Token : " + loginResult.getAccessToken().getToken());
+                Logger.d("SigninActivity", "Userid : " + loginResult.getAccessToken().getUserId());
+                Logger.d("SigninActivity", "Permission List : " + loginResult.getAccessToken().getPermissions() + "");
+                Logger.d("SigninActivity", "id : " + loginResult.getAccessToken().getUserId());
 
 
                 //loginResult.getAccessToken() 정보를 가지고 유저 정보를 가져올수 있습니다.
@@ -57,7 +54,7 @@ public class SigninActivity extends AppCompatActivity {
                             @Override
                             public void onCompleted(JSONObject object, GraphResponse response) {
                                 try {
-                                    Log.e("user profile", object.toString());
+                                    Logger.e("user profile", object.toString());
                                 } catch (Exception e) {
                                     e.printStackTrace();
                                 }
