1793045b8b6b55f0755f0ca8a8de6e04fcf02d02
timejp <tokijh@naver.com>
2017-03-30 03:03:17 +0900

    Append Google Sign in (See #27)

--- app/src/main/java/com/team3/fastcampus/record/Account/SigninActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/Account/SigninActivity.java
@@ -6,40 +6,41 @@ package com.team3.fastcampus.record.Account;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.team3.fastcampus.record.R;
 import com.team3.fastcampus.record.Util.Logger;
 
-import org.json.JSONObject;
-
 /**
  * 회원 로그인 Activity
  */
-public class SigninActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int RC_SIGN_IN = 9001;
+    public static final String TAG = "SigninActivity";
 
-    CallbackManager callbackManager;
-    LoginButton loginButtonFacebook;
-    SignInButton loginButtonGoogle;
+    private static final int REQ_GOOGLE_SIGNIN = 9001;
+
+    private CallbackManager facebookOnActivityResult;
+
+    private Button btn_SignIn;
+    private Button btn_SignUp;
+    private LoginButton btn_Facebook;
+    private SignInButton btn_Google;
+
     private GoogleApiClient mGoogleApiClient;
 
 
@@ -48,118 +49,120 @@ public class SigninActivity extends AppCompatActivity implements GoogleApiClient
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        callbackManager = CallbackManager.Factory.create();
+        init();
+    }
 
-        // [START configure_signin]
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+    private void init() {
+        initView();
+        initListener();
+        settingFacebook();
+        settingGoogle();
+    }
+
+    private void initView() {
+        btn_SignIn = (Button) findViewById(R.id.btn_signin);
+        btn_SignUp = (Button) findViewById(R.id.btn_signup);
+        btn_Google = (SignInButton) findViewById(R.id.btn_google);
+        btn_Facebook = (LoginButton) findViewById(R.id.btn_facebook);
+    }
+
+    private void initListener() {
+        btn_SignIn.setOnClickListener(this);
+        btn_SignUp.setOnClickListener(this);
+        btn_Google.setOnClickListener(this);
+    }
+
+    private void settingFacebook() {
+        facebookOnActivityResult = CallbackManager.Factory.create();
+
+        btn_Facebook.setReadPermissions("public_profile", "email");
+        btn_Facebook.registerCallback(facebookOnActivityResult, facebook_callback);
+    }
+
+    private void settingGoogle() {
+        btn_Google.setSize(SignInButton.SIZE_STANDARD);
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-        // [END configure_signin]
 
-
-        // [START build_client]
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .enableAutoManage(this, google_callbackManager)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-        // [END build_client]
-
-
-        loginButtonGoogle = (SignInButton) findViewById(R.id.login_button_google);
-        loginButtonGoogle.setSize(SignInButton.SIZE_STANDARD);
-//        loginButtonGoogle.setScopes(gso.getScopeArray());
-        loginButtonGoogle.setOnClickListener(new View.OnClickListener() {
-                @Override
-            public void onClick(View v) {
-
-                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-                startActivityForResult(signInIntent, RC_SIGN_IN);
-
-            }
-        });
-
-        loginButtonFacebook = (LoginButton) findViewById(R.id.login_button_facebook);
-        loginButtonFacebook.setReadPermissions("public_profile", "email");
-        loginButtonFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Logger.d("SigninActivity", "Token : " + loginResult.getAccessToken().getToken());
-                Logger.d("SigninActivity", "Userid : " + loginResult.getAccessToken().getUserId());
-                Logger.d("SigninActivity", "Permission List : " + loginResult.getAccessToken().getPermissions() + "");
-                Logger.d("SigninActivity", "id : " + loginResult.getAccessToken().getUserId());
-
-
-                //loginResult.getAccessToken() 정보를 가지고 유저 정보를 가져올수 있습니다.
-                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
-                        new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(JSONObject object, GraphResponse response) {
-                                try {
-                                    Logger.e("user profile", object.toString());
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                request.executeAsync();
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
-
-
     }
 
-
-    public void btnSignup(View view) {
-        Intent intent = new Intent(this, SignupActivity.class);
-        startActivity(intent);
+    private void successFacebook(LoginResult loginResult) {
+        Logger.d(TAG, "Token : " + loginResult.getAccessToken().getToken());
+        Logger.d(TAG, "Userid : " + loginResult.getAccessToken().getUserId());
+        Logger.d(TAG, "Permission List : " + loginResult.getAccessToken().getPermissions() + "");
+        Logger.d(TAG, "id : " + loginResult.getAccessToken().getUserId());
+        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), (object, response) -> {
+                    Logger.e("user profile", object.toString());
+                }
+        );
+        request.executeAsync();
     }
 
+    private void successGoogle(GoogleSignInAccount result) {
+        Logger.d(TAG, result.getDisplayName());
+        Logger.d(TAG, result.getEmail());
+    }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btn_signin:
+                break;
+            case R.id.btn_signup:
+                intent = new Intent(this, SignupActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.login_button_google:
+                intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(intent, REQ_GOOGLE_SIGNIN);
+                break;
+        }
+    }
+
+    private GoogleApiClient.OnConnectionFailedListener google_callbackManager = (connectionResult) -> {
+        Logger.e(TAG, connectionResult.getErrorCode() + " " + connectionResult.getErrorMessage());
+    };
+
+    private FacebookCallback<LoginResult> facebook_callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            successFacebook(loginResult);
+        }
+
+        @Override
+        public void onCancel() {
+            Logger.i(TAG, "facebook onCancel");
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+            Logger.e(TAG, error.getMessage());
+        }
+    };
+
+    private void googleOnActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQ_GOOGLE_SIGNIN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                successGoogle(result.getSignInAccount());
+            } else {
+                Logger.e(TAG, "google sign in is not success!");
+            }
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-
-
-        // Result returned from launching the Intent from
-        //   GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount acct = result.getSignInAccount();
-                // Get account information
-//                mFullName = acct.getDisplayName();
-//                mEmail = acct.getEmail();
-            }
-        }
-
-        }
-
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d("TAG", "onConnectionFailed:" + connectionResult);
-
-
-
+        facebookOnActivityResult.onActivityResult(requestCode, resultCode, data);
+        googleOnActivityResult(requestCode, resultCode, data);
     }
+
 }
-
-
-
-
