19d25e14f01000139bdcaac093597b881827a168
kyuwankim <lpnids@gmail.com>
2017-04-04 03:13:06 +0900

    for DiaryWriteDetail 1. add DiaryWriteDetailActivity 2. add activity_diary_write_detail.xml 3. list_view_item_write_detail_menu.xml (ActionBar)
    4.remove fragment_diary_list_view_item_write_detail.xml

---
+++ app/src/main/java/com/team3/fastcampus/record/Diary/Domain/DiaryWriteDetailActivity.java
@@ -0,0 +1,15 @@
+package com.team3.fastcampus.record.Diary.Domain;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team3.fastcampus.record.R;
+
+public class DiaryWriteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_write_detail);
+    }
+}
