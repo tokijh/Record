c57be55e6e42c8d97e6075cc377a9466662b9ecd
timejp <tokijh@naver.com>
2017-04-04 23:03:40 +0900

    InDiaryVireFragment TabLayout setting (See #51)

--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryViewFragment.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryViewFragment.java
@@ -5,7 +5,11 @@ package com.team3.fastcampus.record.InDiary;
  */
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +21,17 @@ import com.team3.fastcampus.record.R;
  */
 public class InDiaryViewFragment extends Fragment {
 
+    private View view;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    // Fragment
+    private InDiaryListViewFragment inDiaryListViewFragment;
+    private InDiaryMapViewFragment inDiaryMapViewFragment;
+
+    // Adaper
+    private InDiaryViewPagerAdapter inDiaryViewPagerAdapter;
 
     public InDiaryViewFragment() {
         // Required empty public constructor
@@ -26,8 +41,66 @@ public class InDiaryViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_diary_view, container, false);
+        view = inflater.inflate(R.layout.fragment_in_diary_view, container, false);
+
+        initView();
+
+        initAdapter();
+
+        initListener();
+
+        initFragment();
+
+        return view;
     }
 
+    private void initView() {
+        tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+    }
+
+    private void initAdapter() {
+        inDiaryViewPagerAdapter = new InDiaryViewPagerAdapter(getActivity().getSupportFragmentManager());
+        viewPager.setAdapter(inDiaryViewPagerAdapter);
+    }
+
+    private void initListener() {
+        // 1. Pager가 변경 되었을 때 Tab를 바꿔주는 리스너
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        // 2. Tab이 변경 되었을 때 페이지를 바꿔주는 리스너
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    }
+
+    private void initFragment() {
+        inDiaryListViewFragment = new InDiaryListViewFragment();
+        inDiaryMapViewFragment = new InDiaryMapViewFragment();
+    }
+
+    class InDiaryViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public static final int TAB_COUNT = 2;
+
+        public InDiaryViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0: // Show List
+                    fragment = inDiaryListViewFragment;
+                    break;
+                case 1: // Show Map
+                    fragment = inDiaryMapViewFragment;
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
+    }
 }
