bea94c5953a2b782173bf2a7fa057d50ea31d2b1
kyuwankim <lpnids@gmail.com>
2017-04-06 13:26:00 +0900

    Add Function DatePicker Dialog

--- app/src/main/java/com/team3/fastcampus/record/Diary/DiaryWriteDetailActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/Diary/DiaryWriteDetailActivity.java
@@ -1,47 +1,81 @@
 package com.team3.fastcampus.record.Diary;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.team3.fastcampus.record.R;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class DiaryWriteDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText et_location;
+    TextView tv_date;
     FloatingActionButton btn_add_photo, btn_add, btn_update, btn_delete;
+    int mYear, mMonth, mDay, mHour, mMinute;
+    DatePickerDialog.OnDateSetListener mDateSetListener;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_write_detail);
 
-
-
-        et_location = (EditText) findViewById(R.id.diary_list_detail_et_diary_locataion);
+        tv_date = (TextView) findViewById(R.id.diary_list_detail_tv_diary_date);
         btn_add = (FloatingActionButton) findViewById(R.id.fab_add);
         btn_add_photo = (FloatingActionButton) findViewById(R.id.fab_photo);
         btn_update = (FloatingActionButton) findViewById(R.id.fab_update);
         btn_delete = (FloatingActionButton) findViewById(R.id.fab_delete);
 
-        et_location.setOnClickListener(this);
+        tv_date.setOnClickListener(this);
         btn_add.setOnClickListener(this);
         btn_add_photo.setOnClickListener(this);
         btn_update.setOnClickListener(this);
         btn_delete.setOnClickListener(this);
 
+        //날자, 시간 가져오기
+        Calendar cal = new GregorianCalendar();
+        mYear = cal.get(Calendar.YEAR);
+        mMonth = cal.get(Calendar.MONTH);
+        mDay = cal.get(Calendar.DAY_OF_MONTH);
+        mHour = cal.get(Calendar.HOUR_OF_DAY);
+        mMinute = cal.get(Calendar.MINUTE);
+
+        UpdateCal();
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                mYear = year;
+                mMonth = month;
+                mDay = dayOfMonth;
+
+                UpdateCal();
+            }
+        };
+
+    }
+
+    private void UpdateCal() {
+        tv_date.setText(String.format("%d / %d / %d",mYear,mMonth +1, mDay));
     }
 
     @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.diary_list_detail_et_diary_locataion:
+            case R.id.diary_list_detail_tv_diary_date:
 
-                //DatePicker 부분을 Dialog로 띄워서 표현해야함
+                new DatePickerDialog(this , mDateSetListener, mYear, mMonth, mDay).show();
 
                 break;
 
@@ -69,9 +103,10 @@ public class DiaryWriteDetailActivity extends AppCompatActivity implements View.
 
     }
 
+}
 
 
-
-
+class PickerDialogSet extends Activity {
+    int mYear, MMonth, mDay, mHour, mMinute;
 
 }
