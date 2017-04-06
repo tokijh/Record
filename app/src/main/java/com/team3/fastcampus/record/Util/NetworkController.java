debfb62fab0d52ae11a103f8321643e6bf80451b
timejp <tokijh@naver.com>
2017-04-06 17:52:48 +0900

    Set En, Decode to static method (See #60)

--- app/src/main/java/com/team3/fastcampus/record/Util/NetworkController.java
+++ app/src/main/java/com/team3/fastcampus/record/Util/NetworkController.java
@@ -96,7 +96,7 @@ public class NetworkController {
      * @return ClassData
      * @throws JsonSyntaxException
      */
-    public <T> T decode(Class<T> clazz, String json) throws JsonSyntaxException {
+    public static <T> T decode(Class<T> clazz, String json) throws JsonSyntaxException {
         return new Gson().fromJson(json, clazz);
     }
 
@@ -107,7 +107,7 @@ public class NetworkController {
      * @return JSONObject
      * @throws JSONException
      */
-    public JSONObject decode(String json) throws JSONException {
+    public static JSONObject decode(String json) throws JSONException {
         return new JSONObject(json);
     }
 
@@ -118,7 +118,7 @@ public class NetworkController {
      * @return
      * @throws JSONException
      */
-    public Map<String, Object> encode(JSONObject jsonObject) throws JSONException {
+    public static Map<String, Object> encode(JSONObject jsonObject) throws JSONException {
         Map<String, Object> data = new HashMap<>();
         Iterator<String> keys = jsonObject.keys();
         while (keys.hasNext()) {
@@ -135,7 +135,7 @@ public class NetworkController {
      * @return
      * @throws JSONException
      */
-    public <T> Map<String, Object> encode(T object) throws JSONException {
+    public static <T> Map<String, Object> encode(T object) throws JSONException {
         return encode(new Gson().toJson(object));
     }
 
@@ -146,7 +146,7 @@ public class NetworkController {
      * @return
      * @throws JSONException
      */
-    public Map<String, Object> encode(String json) throws JSONException {
+    public static Map<String, Object> encode(String json) throws JSONException {
         return encode(new JSONObject(json));
     }
 
