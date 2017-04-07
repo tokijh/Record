c8d5af6eaac6574065823920e78030d237f411d6
timejp <tokijh@naver.com>
2017-04-08 01:51:55 +0900

    Show MapView (See #72)

--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryMapViewFragment.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryMapViewFragment.java
@@ -4,30 +4,107 @@ package com.team3.fastcampus.record.InDiary;
  * Created by yoonjoonghyun on 2017. 3. 25..
  */
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.team3.fastcampus.record.R;
 
 /**
  * InDiary의 리스트를 Map으로 보여주기 위한 Fragment
  */
-public class InDiaryMapViewFragment extends Fragment {
+public class InDiaryMapViewFragment extends Fragment implements OnMapReadyCallback  {
 
+    private Context context;
+
+    private View view;
+
+    private GoogleMap googleMap;
+    private MapView mapView;
 
     public InDiaryMapViewFragment() {
-        // Required empty public constructor
+
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_diary_map_view, container, false);
+        if (view != null) {
+            return view;
+        }
+        view = inflater.inflate(R.layout.fragment_in_diary_map_view, container, false);
+
+        initView();
+
+        initGoogleMap(savedInstanceState);
+
+        return view;
     }
 
+    private void initView() {
+        mapView = (MapView) view.findViewById(R.id.map);
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState) {
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume();
+
+        MapsInitializer.initialize(getActivity().getApplicationContext());
+
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        LatLng seoul = new LatLng(37.566535, 126.97796);
+        this.googleMap.addMarker(new MarkerOptions().position(seoul));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 15));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
