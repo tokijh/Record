93219bd8e9baf62d1e8c921cb251b79742e43349
timejp <tokijh@naver.com>
2017-04-11 04:14:51 +0900

    Diary를 DB에 저장시키기 위해 RealmObject를 상속받는다. (See #92)

--- app/src/main/java/com/team3/fastcampus/record/Diary/Model/Diary.java
+++ app/src/main/java/com/team3/fastcampus/record/Diary/Model/Diary.java
@@ -1,29 +1,34 @@
 package com.team3.fastcampus.record.Diary.Model;
 
-import com.team3.fastcampus.record.InDiary.Model.InDiary;
-import com.team3.fastcampus.record.Model.User;
-
-import java.util.List;
+import io.realm.RealmObject;
 
 /**
  * Created by tokijh on 2017. 3. 31..
  */
 
-public class Diary {
+public class Diary extends RealmObject {
+    public String username;
     public long pk;
     public String title;
-    public User author;
     public String created_date;
 
     public Diary() {
 
     }
 
-    public Diary(long pk, String title, User author, String created_date) {
+    public Diary(String username, long pk, String title, String created_date) {
+        this.username = username;
         this.pk = pk;
         this.title = title;
-        this.author = author;
         this.created_date = created_date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public long getPk() {
@@ -40,14 +45,6 @@ public class Diary {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
     }
 
     public String getCreated_date() {
