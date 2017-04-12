bf9b2ff19f6ef3a28dfac6281fdaab3818046e7a
timejp <tokijh@naver.com>
2017-04-12 21:36:04 +0900

    To save InDiary image into DB (See #92)

---
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/Model/ImageData.java
@@ -0,0 +1,12 @@
+package com.team3.fastcampus.record.InDiary.Model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by tokijh on 2017. 4. 12..
+ */
+
+public class ImageData extends RealmObject {
+    public String url;
+    public byte[] data;
+}
