728811446a6aad1949026455c5825b475ff38de8
kyuwankim <lpnids@gmail.com>
2017-04-18 00:43:02 +0900

    Add VideoView SignIn Activity

--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryManageActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryManageActivity.java
@@ -6,17 +6,36 @@ package com.team3.fastcampus.record.InDiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.team3.fastcampus.record.R;
 
 /**
  * InDiary 관리(추가 및 수정) Activity
  */
-public class InDiaryManageActivity extends AppCompatActivity {
+public class InDiaryManageActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_in_diary_manage);
+        setContentView(R.layout.activity_in_diary_manage_item);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.fab_add:
+
+                break;
+            case R.id.fab_update:
+
+                break;
+            case R.id.fab_delete:
+
+                break;
+        }
     }
 }
