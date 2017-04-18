566c752573fbb8ea9b0e9472cd6e762382b257ed
timejp <tokijh@naver.com>
2017-04-19 02:06:34 +0900

    DiaryManage Create (See #128)

--- app/src/main/java/com/team3/fastcampus/record/Diary/DiaryManageActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/Diary/DiaryManageActivity.java
@@ -3,7 +3,6 @@ package com.team3.fastcampus.record.Diary;
 import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -12,53 +11,65 @@ import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.team3.fastcampus.record.Diary.Model.Diary;
 import com.team3.fastcampus.record.R;
+import com.team3.fastcampus.record.Util.LocationPicker;
 import com.team3.fastcampus.record.Util.Permission.PermissionController;
+import com.team3.fastcampus.record.Util.RealmDatabaseManager;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class DiaryManageActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = "DiaryManageActivity";
+
+    public static final int MODE_VIEW = 0;
+    public static final int MODE_CREATE = 1;
+    public static final int MODE_EDIT = 2;
+    public static final int MODE_DELETE = 3;
+
     private final int REQ_CAMERA = 101; //카메라요청코드
     private final int REQ_GALLERY = 102; //갤러리요청코드
 
+    private int MODE = MODE_VIEW;
+    private long PK = -1;
+    private Diary diary;
+
     Uri fileUri = null;
 
-    TextView tv_date, tv_endDate;
-    FloatingActionButton btn_add, btn_update, btn_delete;
+    TextView tv_date, tv_endDate, tv_location;
+    EditText ed_title;
+    FloatingActionMenu fab_view;
+    FloatingActionButton fab_view_btn_update, fab_view_btn_delete;
+    FloatingActionMenu fab_edit;
+    FloatingActionButton fab_edit_btn_save, fab_edit_btn_cancel;
     int mYear, mMonth, mDay, mHour, mMinute;
     int mEndYear, mEndMonth, mEndDay;
     ImageView imageView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_manage);
 
-        new PermissionController(this,
-                new String[]{
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.CAMERA})
-                .check(new PermissionController.PermissionCallback() {
-                    @Override
-                    public void success() {
-                        init();
-                    }
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null && bundle.containsKey("MODE") && bundle.containsKey("PK")) {
+            MODE = bundle.getInt("MODE");
+            PK = bundle.getLong("PK");
+        }
 
-                    @Override
-                    public void error() {
-                        Toast.makeText(DiaryManageActivity.this, "권한을 허용하지 않으면 프로그램을 실행할 수 없습니다", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                });
+        init();
+
+        initByMode();
     }
 
     private void init() {
@@ -72,18 +83,25 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
     private void initView() {
         tv_date = (TextView) findViewById(R.id.diary_list_detail_tv_diary_date);
         tv_endDate = (TextView) findViewById(R.id.diary_list_detail_tv_diary_end_date);
-        btn_add = (FloatingActionButton) findViewById(R.id.fab_add);
-        btn_update = (FloatingActionButton) findViewById(R.id.fab_update);
-        btn_delete = (FloatingActionButton) findViewById(R.id.fab_delete);
+        tv_location = (TextView) findViewById(R.id.diary_list_detail_tv_diary_locataion);
+        ed_title = (EditText) findViewById(R.id.diary_list_detail_et_diary_title);
+        fab_view = (FloatingActionMenu) findViewById(R.id.fab_view);
+        fab_view_btn_update = (FloatingActionButton) findViewById(R.id.fab_view_update);
+        fab_view_btn_delete = (FloatingActionButton) findViewById(R.id.fab_view_delete);
+        fab_edit = (FloatingActionMenu) findViewById(R.id.fab_edit);
+        fab_edit_btn_save = (FloatingActionButton) findViewById(R.id.fab_edit_save);
+        fab_edit_btn_cancel = (FloatingActionButton) findViewById(R.id.fab_edit_cancel);
         imageView = (ImageView) findViewById(R.id.diary_list_detail_image);
     }
 
     private void initListener() {
         tv_date.setOnClickListener(this);
         tv_endDate.setOnClickListener(this);
-        btn_add.setOnClickListener(this);
-        btn_update.setOnClickListener(this);
-        btn_delete.setOnClickListener(this);
+        tv_location.setOnClickListener(this);
+        fab_view_btn_update.setOnClickListener(this);
+        fab_view_btn_delete.setOnClickListener(this);
+        fab_edit_btn_save.setOnClickListener(this);
+        fab_edit_btn_cancel.setOnClickListener(this);
         imageView.setOnClickListener(this);
     }
 
@@ -100,11 +118,78 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
         updateDateText();
     }
 
+    private void initByMode() {
+        switch (MODE) {
+            case MODE_VIEW:
+                modeView();
+                break;
+            case MODE_CREATE:
+                modeCreate();
+                break;
+            case MODE_EDIT:
+                modeEdit();
+                break;
+            case MODE_DELETE:
+                modeDelete();
+                break;
+        }
+    }
+
+    private void modeView() {
+        fab_view.setVisibility(View.VISIBLE);
+        fab_edit.setVisibility(View.GONE);
+        RealmDatabaseManager realmDatabaseManager = RealmDatabaseManager.getInstance();
+        diary = realmDatabaseManager.get(Diary.class)
+                .equalTo("pk", PK)
+                .findFirst();
+
+        // TODO 시작, 종료날짜, Location Text 표시
+//        tv_date.setText();
+//        tv_endDate.setText();
+//        tv_location.setText();
+        ed_title.setText(diary.title);
+        ed_title.setEnabled(false);
+    }
+
+    private void modeCreate() {
+        fab_view.setVisibility(View.GONE);
+        fab_edit.setVisibility(View.VISIBLE);
+    }
+
+    private void modeEdit() {
+        fab_view.setVisibility(View.GONE);
+        fab_edit.setVisibility(View.VISIBLE);
+        modeView();
+        ed_title.setEnabled(true);
+    }
+
+    private void modeDelete() {
+        new AlertDialog.Builder(DiaryManageActivity.this)
+                .setMessage("정말로 삭제 하겠습니까?")
+                .setPositiveButton("확인",
+                        (dialog, which) -> deleteDiary())
+                .setNegativeButton("취소",
+                        (dialog, which) -> finish())
+                .create()
+                .show();
+    }
+
+    private void deleteDiary() {
+        RealmDatabaseManager realmDatabaseManager = RealmDatabaseManager.getInstance();
+        realmDatabaseManager.delete(Diary.class, realmResults -> realmResults.equalTo("pk", PK).findAll());
+    }
+
+    private void saveDiary() {
+
+    }
+
     private void updateDateText() {
+        // TODO diary에 시작날자 저장 mYear, mMonth, mDay...
         tv_date.setText(String.format("%d/%d/%d", mYear, mMonth + 1, mDay));
     }
 
     private void upEndDateDateText() {
+        // TODO diary에 종료날자 저장 mEndYear, mEndMonth, mEndDay...
         tv_endDate.setText(String.format("%d/%d/%d", mEndYear, mEndMonth + 1, mEndDay));
     }
 
@@ -116,12 +201,16 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
         new DatePickerDialog(this, mEndDateSetListener, mYear, mMonth, mDay).show();
     }
 
-    private void actionAdd() {
-        Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
+    private void showLocationPicker() {
+        new LocationPicker(this).show((address, location) -> {
+            String addressStr = address.getCountryName() + " " + address.getFeatureName();
+            tv_location.setText(addressStr);
+            // TODO diary에 위치 저장
+            // diary.location = location;
+        });
     }
 
     private void actionPhoto() {
-        Toast.makeText(this, "photo", Toast.LENGTH_SHORT).show();
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -137,14 +226,6 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
         startActivityForResult(intent, REQ_CAMERA);
     }
 
-    private void actionUpdate() {
-        Toast.makeText(this, "update", Toast.LENGTH_SHORT).show();
-    }
-
-    private void actionDelete() {
-        Toast.makeText(this, "delete", Toast.LENGTH_SHORT).show();
-    }
-
     private void actionGallery() {
         Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show();
 
@@ -153,24 +234,29 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQ_GALLERY);
     }
 
-    private void actionCameraSelect() {
-        AlertDialog.Builder alert_confirm = new AlertDialog.Builder(DiaryManageActivity.this);
-        alert_confirm.setMessage("Select Camera").setCancelable(false).setPositiveButton("camera",
-                new DialogInterface.OnClickListener() {
+    private void actionPhotoSelect() {
+        new PermissionController(this,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA})
+                .check(new PermissionController.PermissionCallback() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        actionPhoto();
+                    public void success() {
+                        new AlertDialog.Builder(DiaryManageActivity.this)
+                                .setMessage("Select Camera")
+                                .setPositiveButton("camera",
+                                        (dialog, which) -> actionPhoto())
+                                .setNegativeButton("gallery",
+                                        (dialog, which) -> actionGallery())
+                                .create()
+                                .show();
                     }
-                }).setNegativeButton("gallery",
-                new DialogInterface.OnClickListener() {
+
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        actionGallery();
-                        return;
+                    public void error() {
+                        Toast.makeText(DiaryManageActivity.this, "권한을 허용하지 않으면 이 기능을 이용 할 수 없습니다.", Toast.LENGTH_SHORT).show();
                     }
                 });
-        AlertDialog alert = alert_confirm.create();
-        alert.show();
     }
 
     @Override
@@ -182,19 +268,15 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
             case R.id.diary_list_detail_tv_diary_end_date:
                 showEndDatePicker();
                 break;
-            case R.id.fab_add:
-                actionAdd();
-                break;
-            case R.id.fab_update:
-                actionUpdate();
-                break;
-            case R.id.fab_delete:
-                actionDelete();
+            case R.id.diary_list_detail_tv_diary_locataion:
+                showLocationPicker();
                 break;
             case R.id.diary_list_detail_image:
-                actionCameraSelect();
+                actionPhotoSelect();
                 break;
-
+            case R.id.fab_edit_save:
+                saveDiary();
+                break;
         }
     }
 
@@ -226,7 +308,6 @@ public class DiaryManageActivity extends AppCompatActivity implements View.OnCli
                         fileUri = data.getData();
                         Glide.with(this).load(fileUri)
                                 .into(imageView);
-
                     }
                     break;
             }
