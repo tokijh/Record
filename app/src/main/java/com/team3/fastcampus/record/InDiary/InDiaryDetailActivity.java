cf8445b7702b134655b1adaf2f943229ddd8fb07
kyuwankim <lpnids@gmail.com>
2017-04-20 22:42:34 +0900

    Add IndiaryDetailActivity (See #136)

--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
@@ -1,13 +1,22 @@
 package com.team3.fastcampus.record.InDiary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team3.fastcampus.record.R;
 
 public class InDiaryDetailActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_diary_detail);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+
     }
 }
