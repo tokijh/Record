057511ebffa13189d0b9938bca5925e793c8cc3d
kyuwankim <lpnids@gmail.com>
2017-04-20 18:04:50 +0900

    Add InDiaryDetailActivity.java , activity_in_diary_detail.xml

---
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.team3.fastcampus.record.InDiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InDiaryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_diary_detail);
+    }
+}
