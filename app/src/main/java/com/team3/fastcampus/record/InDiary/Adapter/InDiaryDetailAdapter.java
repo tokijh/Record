60cd7d75f052fd3a513453c52da06b699546d509
kyuwankim <lpnids@gmail.com>
2017-04-21 02:30:44 +0900

    Add IndiaryDetailActivity (See #136)

--- app/src/main/java/com/team3/fastcampus/record/InDiary/Adapter/InDiaryDetailAdapter.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/Adapter/InDiaryDetailAdapter.java
@@ -1,12 +1,17 @@
 package com.team3.fastcampus.record.InDiary.Adapter;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.team3.fastcampus.record.InDiary.Model.Image;
 import com.team3.fastcampus.record.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by kimkyuwan on 2017. 4. 20..
@@ -14,25 +19,13 @@ import com.team3.fastcampus.record.R;
 
 public class InDiaryDetailAdapter extends PagerAdapter {
 
+    ArrayList<Image> datas;
+    Context context;
     LayoutInflater inflater;
 
-    public InDiaryDetailAdapter(LayoutInflater inflater) {
-        this.inflater = inflater;
-    }
-
-    @Override
-    public Object instantiateItem(ViewGroup container, int position) {
-        //return super.instantiateItem(container, position);
-
-        View view = inflater.inflate(R.layout.activity_in_diary_detail_item, null);
-        ImageView imageView = (ImageView) view.findViewById(R.id.in_diary_detail_item_imageview);
-
-        imageView.setImageResource(R.drawable.cast_abc_scrubber_control_off_mtrl_alpha+position);
-        container.addView(view);
-
-
-        return view;
-
+    public InDiaryDetailAdapter(Context context) {
+        this.context = context;
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -42,17 +35,26 @@ public class InDiaryDetailAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-
-
         return view == object;
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        super.destroyItem(container, position, object);
+    public Object instantiateItem(ViewGroup container, int position) {
+        //return super.instantiateItem(container, position);
 
-        container.removeView((View) object);
+        View view = inflater.inflate(R.layout.activity_in_diary_detail_item, null);
+        ImageView imageView = (ImageView)view.findViewById(R.id.in_diary_detail_item_imageview);
+        Image image = datas.get(position);
+
+        Glide.with(context).load(image.photo).placeholder(R.drawable.no_photo).into(imageView);
+
+        container.addView(view);
+        return view;
     }
 
-
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        //super.destroyItem(container, position, object);
+        container.removeView((View)object);
+    }
 }
--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
@@ -3,25 +3,36 @@ package com.team3.fastcampus.record.InDiary;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.team3.fastcampus.record.InDiary.Adapter.InDiaryDetailAdapter;
+import com.team3.fastcampus.record.InDiary.Model.Image;
 import com.team3.fastcampus.record.R;
+
+import java.util.ArrayList;
 
 public class InDiaryDetailActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     InDiaryDetailAdapter adapter;
+    ArrayList<Image> datas;
+
+    TextView tv_content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_diary_detail);
 
+
+        tv_content = (TextView) findViewById(R.id.in_diary_detail_content_textview);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
-        adapter = new InDiaryDetailAdapter(getLayoutInflater());
-        viewPager.setAdapter(adapter);
-        viewPager.setCurrentItem(0);
+        adapter = new InDiaryDetailAdapter(InDiaryDetailActivity.this);
+        //viewPager.setAdapter(adapter);
+
+        tv_content.setText("Test");
     }
+
 
 
 }
