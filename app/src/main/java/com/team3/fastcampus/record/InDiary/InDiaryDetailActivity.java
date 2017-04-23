054893c135aeebdf864a151c50679962e6f8bc49
kyuwankim <lpnids@gmail.com>
2017-04-23 23:04:25 +0900

    Add IndiaryDetailActivity (See #136)

--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
@@ -3,6 +3,8 @@ package com.team3.fastcampus.record.InDiary;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.team3.fastcampus.record.InDiary.Adapter.InDiaryDetailAdapter;
@@ -16,7 +18,7 @@ public class InDiaryDetailActivity extends AppCompatActivity {
     ViewPager viewPager;
     InDiaryDetailAdapter adapter;
     ArrayList<Image> datas;
-
+    RelativeLayout relativeLayout;
     TextView tv_content;
 
     @Override
@@ -25,12 +27,29 @@ public class InDiaryDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_in_diary_detail);
 
 
+        relativeLayout = (RelativeLayout) findViewById(R.id.in_diary_detail_content_area);
         tv_content = (TextView) findViewById(R.id.in_diary_detail_content_textview);
         viewPager = (ViewPager) findViewById(R.id.in_diary_list_view_viewPager);
         adapter = new InDiaryDetailAdapter(InDiaryDetailActivity.this);
-        viewPager.setAdapter(adapter);
 
-        tv_content.setText("Test");
+       // tv_content.setVisibility(View.INVISIBLE);
+       // relativeLayout.setVisibility(View.INVISIBLE);
+
+
+        viewPager.setAdapter(adapter);
+        viewPager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setVisibility(View.VISIBLE);
+                tv_content.setVisibility(View.VISIBLE);
+                tv_content.setText("Test");
+            }
+        });
+
+
+
+
+
     }
 
 
