054e1f527b765aaf5687105d5154370d72c954d3
kyuwankim <lpnids@gmail.com>
2017-04-24 01:22:45 +0900

    Add IndiaryDetailActivity (See #136)

--- app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
+++ app/src/main/java/com/team3/fastcampus/record/InDiary/InDiaryDetailActivity.java
@@ -6,51 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.team3.fastcampus.record.InDiary.Adapter.InDiaryDetailAdapter;
-import com.team3.fastcampus.record.InDiary.Model.Image;
 import com.team3.fastcampus.record.R;
-
-import java.util.ArrayList;
 
 public class InDiaryDetailActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     InDiaryDetailAdapter adapter;
-    ArrayList<Image> datas;
-    RelativeLayout relativeLayout;
     TextView tv_content;
-
+    RelativeLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_diary_detail);
 
-
-        relativeLayout = (RelativeLayout) findViewById(R.id.in_diary_detail_content_area);
+        layout = (RelativeLayout) findViewById(R.id.in_diary_detail_content_area);
         tv_content = (TextView) findViewById(R.id.in_diary_detail_content_textview);
         viewPager = (ViewPager) findViewById(R.id.in_diary_list_view_viewPager);
         adapter = new InDiaryDetailAdapter(InDiaryDetailActivity.this);
-
-       // tv_content.setVisibility(View.INVISIBLE);
-       // relativeLayout.setVisibility(View.INVISIBLE);
-
-
         viewPager.setAdapter(adapter);
-        viewPager.setOnClickListener(new View.OnClickListener() {
+
+        layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                relativeLayout.setVisibility(View.VISIBLE);
-                tv_content.setVisibility(View.VISIBLE);
-                tv_content.setText("Test");
+                Toast.makeText(InDiaryDetailActivity.this, "Touch Layout", Toast.LENGTH_SHORT).show();
+
             }
         });
 
 
+        tv_content.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(InDiaryDetailActivity.this, "Touch TextView", Toast.LENGTH_SHORT).show();
 
-
+            }
+        });
 
     }
+
 
 
 
